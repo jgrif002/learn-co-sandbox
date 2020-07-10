6f1ad93b0da632237ef913f6b85b602295f4ad41
Jesse Griffin <jesse.griffin@gmail.com>
2020-07-10 01:09:09 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,30 @@
+#def greeting
+#  puts "Hello World"
+#end
+
+#def say_greeting_five_times
+#greeting
+#greeting
+#greeting
+#greeting
+#greeting
+#end
+
+#say_greeting_five_times
+
+def greeting_a_programmer(name, language)
+  
+  puts "Hello #{name}.  We heard you are a great #{language} programmer"
+end
+
+greeting_a_programmer("Maria", "Ruby")
+
+greeting_a_programmer("Steven","Elixir")
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+
+stylish_chef
